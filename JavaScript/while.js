b2af6e746d7e8ccc7e583113c980a38e4f7e0c9a
Alex Serrano <alexserranolledo@gmail.com>
2024-05-24 12:21:34 +0200

    mas aclarado el while

--- JavaScript/while.js
+++ JavaScript/while.js
@@ -10,9 +10,14 @@ document.write("Bien dicho")
 var numero1, numero3, numero2;
 
 
-/*numero1 = parseInt(prompt("Primer valor"))
+
+//mas o menos lo entiendo el (!(condicion))
+
+numero1 = parseInt(prompt("Primer valor"))
 numero2 = parseInt(prompt("Segundo valor"))
 numero3 = parseInt(prompt("Entre esos numeros valor"))
-    while (numero1 <= numero3 <= numero2) {
-        document.write(numentremedio)
-    }*/
+while (!(numero1 <= numero3 && numero3 <= numero2)) {
+    numero3 = parseInt(prompt("El tercer número debe estar entre el primero y el segundo. Inténtalo de nuevo."));
+}
+
+document.write("El tercer número está entre el primero y el segundo.");
