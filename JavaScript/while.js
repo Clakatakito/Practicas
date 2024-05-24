2e5681b22e1a02655e9178b850b0928c649dfd78
Alex Serrano <alexserranolledo@gmail.com>
2024-05-24 12:11:58 +0200

    While

---
+++ JavaScript/while.js
@@ -0,0 +1,18 @@
+
+//While
+
+var pregunta
+while (pregunta != "españa" ) {
+    pregunta = prompt("Dime el mejor pais de todos");
+}
+document.write("Bien dicho")
+
+var numero1, numero3, numero2;
+
+
+/*numero1 = parseInt(prompt("Primer valor"))
+numero2 = parseInt(prompt("Segundo valor"))
+numero3 = parseInt(prompt("Entre esos numeros valor"))
+    while (numero1 <= numero3 <= numero2) {
+        document.write(numentremedio)
+    }*/
