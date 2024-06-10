a45dfe502a6ef7ccb947afd808f7fcd3541af1b8
Clakatakito <157515741+Clakatakito@users.noreply.github.com>
2024-06-10 17:58:42 +0200

    Update templete_literal.js

--- JavaScript/templete_literal.js
+++ JavaScript/templete_literal.js
@@ -5,7 +5,9 @@
 
 var marca = 'Toyota';
 var modelo = 'Corolla';
-var mensaje = (`La marca de este coche es ${marca} y el modelo es ${modelo}.`); /*No se puede
-empezar con " " i luego poner dentro `${}` si hay que hacer un templete en algun caso el document.write
+var mensaje = (`La marca de este coche es ${marca} y 
+el modelo es ${modelo}.`); /*No se puede
+empezar con " " i luego poner dentro `${}` si hay que hacer 
+un templete en algun caso el document.write
 hay que empezarlo con ` `*/ 
 document.write(mensaje); // "Toyota Corolla está arrancando."
