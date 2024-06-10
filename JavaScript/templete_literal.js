1c0d2805b455705f3f03e1ca8af803723e95b64c
Alex Serrano <alexserranolledo@gmail.com>
2024-06-10 16:45:52 +0200

    templete literal

---
+++ JavaScript/templete_literal.js
@@ -0,0 +1,9 @@
+
+//Templete literal
+
+//Importante usarlo con ` ` no con " "
+
+var marca = 'Toyota';
+var modelo = 'Corolla';
+var mensaje = `${marca} ${modelo} está arrancando.`;
+document.write(mensaje); // "Toyota Corolla está arrancando."
