242e0f7bb83223755794e0ce7ebb5c4bece56cc4
Alex Serrano <alexserranolledo@gmail.com>
2024-06-10 16:55:21 +0200

    update

--- JavaScript/templete_literal.js
+++ JavaScript/templete_literal.js
@@ -5,5 +5,7 @@
 
 var marca = 'Toyota';
 var modelo = 'Corolla';
-var mensaje = `${marca} ${modelo} está arrancando.`;
+var mensaje = (`La marca de este coche es ${marca} y el modelo es ${modelo}.`); /*No se puede
+empezar con " " i luego poner dentro `${}` si hay que hacer un templete en algun caso el document.write
+hay que empezarlo con ` `*/ 
 document.write(mensaje); // "Toyota Corolla está arrancando."
