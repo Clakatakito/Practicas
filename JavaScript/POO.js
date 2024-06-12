bc0e09624f9fde03ff5118728c3be2d6895f7941
Alex Serrano <alexserranolledo@gmail.com>
2024-06-12 18:15:06 +0200

    update

--- JavaScript/POO.js
+++ JavaScript/POO.js
@@ -16,10 +16,12 @@ const miCoche = new Coche("Toyota", "Corolla");
 // Usar la instancia para mostrar la información
 document.write(`El coche ${miCoche.marca} con modelo ${miCoche.modelo} está arrancando.`);
 
-document.write("<br>")
-document.write("<br>")
-document.write("<br>")
-document.write("<br>")
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+//Ejemplo con las funciones
 
 class Moto {
     constructor(marca, modelo) {
@@ -38,5 +40,45 @@ class Moto {
 
 const miMoto = new Moto('Honda', 'CBR500R');
 miMoto.arrancar();
-document.write("<br>")
+document.write("<br>");
 miMoto.detenerse();
+
+//Con Herencias
+
+class Comanche {
+    // Definición de propiedades privadas usando el prefijo #
+    #kilometraje;
+    
+    constructor(marca, modelo, kilometrajeInicial) {
+        this.marca = marca;    // Propiedad pública
+        this.modelo = modelo;  // Propiedad pública
+        this.#kilometraje = kilometrajeInicial;  // Propiedad privada
+    }
+
+    // Método público para obtener el kilometraje
+    obtenerKilometraje() {
+        return this.#kilometraje;
+    }
+
+    // Método público para incrementar el kilometraje
+    conducir(km) {
+        if (km > 0) {
+            this.#kilometraje += km;
+        } else {
+            document.write("Kilometraje debe ser positivo");
+        }
+    }
+}
+document.write("<br>")
+document.write("<br>")
+const miComanche = new Comanche('Hosa', 'mamatua', 10000);
+
+// Intentando acceder a la propiedad privada (esto dará un error)
+//console.log(miComanche.#kilometraje); Error: Propiedad privada no accesible desde fuera de la clase
+
+// Acceder a la propiedad privada a través de un método público
+document.write("El Kilometraje es " , miComanche.obtenerKilometraje()); // Output: 10000
+
+// Usar el método público para modificar la propiedad privada
+miCoche.conducir(500);
+document.write(miComanche.obtenerKilometraje()); // Output: 10500
