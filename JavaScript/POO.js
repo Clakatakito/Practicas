b5681447fa02526da16a752ab746daeda37a9338
Alex Serrano <alexserranolledo@gmail.com>
2024-07-03 18:20:49 +0200

    No lo he hecho bien pero sabiendo las herencias igual me ayuda

--- JavaScript/POO.js
+++ JavaScript/POO.js
@@ -36,49 +36,28 @@ class Moto {
     detenerse() {
         document.write(`${this.marca} ${this.modelo} se ha detenido.`);
     }
+    get la_marca() {
+        return this.marca
+    }
+    set modificarMarca(nueva_marca) {
+        this._marca = nueva_marca;
+    } 
 }
 
 const miMoto = new Moto('Honda', 'CBR500R');
 miMoto.arrancar();
 document.write("<br>");
 miMoto.detenerse();
+document.write("<br>")
+document.write("<br>")
+document.write(`La marca de la moto es: ${miMoto.la_marca}<br>`);
+
+document.write("<br>")
+document.write("<br>")
+
+miMoto.modificarMarca = "Kamasaki"
+document.write(miMoto.modificarMarca)
 
 //Con Herencias
 
-class Comanche {
-    // Definición de propiedades privadas usando el prefijo #
-    #kilometraje;
-    
-    constructor(marca, modelo, kilometrajeInicial) {
-        this.marca = marca;    // Propiedad pública
-        this.modelo = modelo;  // Propiedad pública
-        this.#kilometraje = kilometrajeInicial;  // Propiedad privada
-    }
 
-    // Método público para obtener el kilometraje
-    obtenerKilometraje() {
-        return this.#kilometraje;
-    }
-
-    // Método público para incrementar el kilometraje
-    conducir(km) {
-        if (km > 0) {
-            this.#kilometraje += km;
-        } else {
-            document.write("Kilometraje debe ser positivo");
-        }
-    }
-}
-document.write("<br>")
-document.write("<br>")
-const miComanche = new Comanche('Hosa', 'mamatua', 10000);
-
-// Intentando acceder a la propiedad privada (esto dará un error)
-//console.log(miComanche.#kilometraje); Error: Propiedad privada no accesible desde fuera de la clase
-
-// Acceder a la propiedad privada a través de un método público
-document.write("El Kilometraje es " , miComanche.obtenerKilometraje()); // Output: 10000
-
-// Usar el método público para modificar la propiedad privada
-miCoche.conducir(500);
-document.write(miComanche.obtenerKilometraje()); // Output: 10500
