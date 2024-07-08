bf83a26bbc42ab2b880583a81c7828c79248a205
Alex Serrano <alexserranolledo@gmail.com>
2024-07-08 19:45:53 +0200

    arreglo2

--- JavaScript/POO.js
+++ JavaScript/POO.js
@@ -1,4 +1,8 @@
+const { JSDOM } = require('jsdom');
 
+const dom = new JSDOM(`<!DOCTYPE html><html><body></body></html>`);
+global.document = dom.window.document;
+global.window = dom.window;
 
 document.write('Hello, Quokka!');
 
