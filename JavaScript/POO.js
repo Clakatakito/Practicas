962243632c8ff3df944ea39ce4339a7b3b762c5e
Alex Serrano <alexserranolledo@gmail.com>
2024-07-08 14:12:25 +0200

    creo librerias.js

--- JavaScript/POO.js
+++ JavaScript/POO.js
@@ -1,4 +1,8 @@
 
+
+
+
+
 //PROGRAMACION ORIENTADA A OBJETOS
 
 
@@ -57,6 +61,70 @@ document.write("<br>")
 
 miMoto.modificarMarca = "Kamasaki"
 document.write("Ahora el nuevo nombre es " , miMoto.modificarMarca)
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+//SUBCLASES
+
+document.write("SUBCLASES")
+document.write("<br>")
+document.write("<br>")
+
+class Personaje{
+    constructor(nombre, edad, pelo) {
+        this.nombre = nombre;
+        this.edad = edad; 
+        this.pelo = pelo;
+    }
+    saludar(){
+        document.write(`${this.nombre} te saluda`)
+    }
+    despedir(){
+        document.write(`${this.nombre} se despide de ti`)
+    }
+    get verNombre(){
+        return this.nombre
+    }
+    set CambiarNombre(NuevoNombre){
+        this.nombre = NuevoNombre
+    }
+}
+
+const personaje1 = new Personaje("Mario", "30", "negro")
+
+personaje1.saludar() //Mario te saluda
+document.write("<br>")
+personaje1.despedir() //Mario se despide de ti
+document.write("<br>")
+document.write(personaje1.verNombre) //Mario
+personaje1.CambiarNombre = "MANOLO"
+document.write("<br>")
+document.write(personaje1.verNombre); //MANOLO
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+class Enemigo extends Personaje {
+    constructor(nombre, edad, pelo, bigote){ //Pondremos los mismos paramentos de la otra clase mas nuevos si queremos
+        super(nombre, edad, pelo) //ponemos aqui los que venga de la clase que estamos tomando, de la que no no
+        this.bigote = bigote
+    }
+    atacar(){
+        document.write(this.nombre, " te ataca")
+    }
+    
+    //no tenemos que hacer un get ni set ya que ya los cojemos de la clse heredada
+}
+
+const enemigo1 = new Enemigo("Bowser", "100", "no", "no")
+
+enemigo1.atacar()
+document.write("<br>")
+enemigo1.saludar() //No tengo ninguna funcion en esta clase llamada saludar(), ya que es de la heredada
+
+
 
 
 
