9a1f2d3ece30bafc143bb5b42abadf30a6769d10
Alex Serrano <alexserranolledo@gmail.com>
2024-10-22 16:07:14 +0200

    arreglos Quokka

--- programacion/JavaScript/archivo_menores/practica.js
+++ programacion/JavaScript/archivo_menores/practica.js
@@ -1,6 +1,3 @@
-document.write("hola");
-document.write("<br>");
-document.write("<br>");
 let si = "hola";
 let no = 54;
 let tio = {eltio: "pepe", edad: 78};
@@ -9,73 +6,60 @@ let letiable2= "laotra";
 let nombre = "alex";
 let apellido = "serrano";
 let nombreful = nombre.concat(" " + apellido);
-document.write(nombreful);
-document.write("Mi nombre es" , " " , nombre);
-document.write("<hr>");
+console.log(nombreful);
+console.log("Mi nombre es" , " " , nombre);
 //putos todos
 /*let tutio = "antonio";
-document.write(tutio);*/
+console.log(tutio);*/
 const constante = 56;
-document.write("La constante es " + constante);
-document.write("<br>");
+console.log("La constante es " + constante);
 //rendondea a decimal
 let precio = Math.round(500.6);
-document.write (precio);
-document.write("<br>");
+console.log (precio);
 //redondea sin mas al siguiente numero
 let precio2 = Math.ceil(347.1);
-document.write(precio2);
-document.write("<br>");
+console.log(precio2);
 //redonde hacia bajo
 let precio3 = Math.floor(700.1);
-document.write(precio3);
-document.write("<br>");
+console.log(precio3);
 let palabra;
 palabra = "palabra";
 //longitud de caracteres de una
-document.write("La longitud de la palabra es " , palabra.length);
-document.write("<br>");
+console.log("La longitud de la palabra es " , palabra.length);
 //la letras del x asta x
-document.write("La letras " , palabra.slice(0,4));
-document.write("<br>");
+console.log("La letras " , palabra.slice(0,4));
 
 //remplazar
 let saludo = "hola manito";
-document.write(saludo.replace("hola" , "ADIOS"));
-document.write("<br>");
+console.log(saludo.replace("hola" , "ADIOS"));
 
 /*let saludo = "hola manito"
 saludo(saludo.replace("hola" , "ADIOS"))
-document.write(saludo)*/
+console.log(saludo)*/
 
 //mayusculas
 
 let mayus;
 mayus = "quiero masyusculas";
-document.write(mayus.toUpperCase());
-document.write("<br>");
+console.log(mayus.toUpperCase());
 
 //minusculas
 let menores
 menores = "Maduras";
-document.write(menores.toLowerCase());
-document.write("<br>");
+console.log(menores.toLowerCase());
 
 //mas concatenar i de forma mas util
 let palabra1con, palabra2con;
 palabra1con = "primera";
 palabra2con = "segunda";
-document.write(palabra1con.concat(" " , palabra2con));
-document.write("<br>");
+console.log(palabra1con.concat(" " , palabra2con));
 
 //acortar (no se visualiza bien)
 let palabra69 = "     Hola     5";
-document.write(palabra69.trimStart());  //para quitar espacios del lado izquierdo, el end para el derecho i el trim ambos
-document.write("<br>");
-document.weite("<br>");
+console.log(palabra69.trimStart());  //para quitar espacios del lado izquierdo, el end para el derecho i el trim ambos
 
 //corta la palabra
-document.write(palabra1con.split("ra"));
+console.log(palabra1con.split("ra"));
 
 const elefate = "elefante" /*las varable con const tienen que inicializarse no se puede hacer
 const cambiar
