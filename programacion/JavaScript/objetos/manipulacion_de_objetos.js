c3db8edaa5498f2005e7bcd96009c5958c502d6a
Alex Serrano <alexserranolledo@gmail.com>
2024-10-26 14:09:55 +0200

    acabe

--- programacion/JavaScript/objetos/manipulacion_de_objetos.js
+++ programacion/JavaScript/objetos/manipulacion_de_objetos.js
@@ -6,38 +6,38 @@ const persona = {
     edad: 18,
     trabajo: false
 }
-console.log(persona)
+console.log(persona);
 //rescribir (da igual si es una variable tipo const)
 persona.trabajo = true
-console.log(persona.trabajo)
+console.log(persona.trabajo);
 
 //añadir
 
-persona.imagen = "imagen.png"
-console.log(persona.imagen)
-console.log(persona)
+persona.imagen = "imagen.png";
+console.log(persona.imagen);
+console.log(persona);
 
 //eliminar
 
-delete persona.imagen
-console.log(persona)
+delete persona.imagen;
+console.log(persona);
 
 //no dejar modificar el objeto
 
 const nomodifica = {
     no: "si",
     si: "no"
-}
-console.log(nomodifica)
-Object.freeze(nomodifica)
-nomodifica.mepica = "si"
-nomodifica.no = "no"
-nomodifica.si = "si"
-console.log(nomodifica)
+};
+console.log(nomodifica);
+Object.freeze(nomodifica);
+nomodifica.mepica = "si";
+nomodifica.no = "no";
+nomodifica.si = "si";
+console.log(nomodifica);
 
 //con object.seal(objeto) si te deja modificar pero no te deja añadir ni eliminar
 
-//destructuring de 2 o mas ob{jetos
+//destructuring de 2 o mas objetos
 
 const producto = {
     nombre: "tablet",
@@ -51,17 +51,48 @@ const cliente = {
     direccion: {
         calle: "calle palomares"
     }
-}
-const {nombre} = producto
-console.log(nombre)
+};
+const {nombre} = producto;
+console.log(nombre);
 /*const {nombre} = cliente //error porque estamos intentando
 hacer 2 variables con el mismo nombre */
-const {nombre: nombrecliente} = cliente
-console.log(nombrecliente)
+const {nombre: nombrecliente} = cliente;
+console.log(nombrecliente);
 
-console.log(cliente.direccion)
-const {direccion} = cliente
-console.log(direccion)
-const {direccion: {calle} } = cliente
+console.log(cliente.direccion);
+const {direccion} = cliente;
+console.log(direccion);
+const {direccion: {calle} } = cliente;
 //const {direccion} = cliente // no es lo mismo eso, te da otro valor
-console.log(calle)
+console.log(calle);
+
+
+//unir
+
+const producto2 = {
+    nombre: "tablet",
+    precio: 100,
+    disponible: true
+};
+
+const carrito = {
+    cantidad: 1,
+    producto2
+};
+console.log(carrito);
+
+const carrito2 = {
+    ejemplo: true,
+    ...producto2 //spread operator
+}
+console.log(carrito2);
+
+const nuevo_objeto = {
+    producto2,
+    carrito
+};
+
+console.log(nuevo_objeto);
+
+const objeto2 = Object.assign(producto2, carrito);
+console.log(objeto2);
