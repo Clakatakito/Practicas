e03dea75949581c704b80e3b461c75c43e99274b
Alex <157515741+Clakatakito@users.noreply.github.com>
2024-11-01 00:43:10 +0100

    Create funciones2.js

---
+++ programacion/JavaScript/funciones/funciones2.js
@@ -0,0 +1,30 @@
+
+suma(1.5, 2);
+
+
+function suma(num1=0, num2=0){
+    console.log(num1 + num2);
+};
+
+
+
+//Function expresion
+
+//console.log(sumar2(3,1)); error
+
+const sumar2 = function(nume1, nume2=0){
+    console.log(nume1 + nume2)
+};
+
+console.log(sumar2(3,1));
+
+
+// Ejemplo de una función flecha con parámetros y valor predeterminado
+
+//sumar(3, 1); error
+
+const sumar = (nume1, nume2 = 0) => {
+    console.log(nume1 + nume2);
+};
+
+sumar(3, 1)
