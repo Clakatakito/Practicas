4f995d9c170920880f18cc6929efa8dc36dce5a3
Clakatakito <alexserranolledo@gmail.com>
2024-11-04 14:01:58 +0100

    luego lo sigo

--- programacion/JavaScript/arrays/array_Methods.js
+++ programacion/JavaScript/arrays/array_Methods.js
@@ -2,3 +2,20 @@
 //Arrays Methods
 
 let arrays = ["HTML", "CSS", "JAVASCRIPT", "PYTHON", "REACT.JS"];
+
+
+//ejemplo con arrow function
+
+const nuevoArray = arrays.filter((tech) => tech != "HTML"); //si es solo un parametro puedes cortar los () //const nuevoArray = arrays.filter(tech => tech != "HTML");
+console.log(nuevoArray);
+
+
+
+//sin arrow function
+
+const tecnologias2 = arrays.filter(function(tech){
+    if(tech != "HTML"){
+        return tech;
+    };
+});
+console.log(tecnologias2)
