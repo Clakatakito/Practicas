19625e0a9a6d5448c25dcec54270aa263c7f8f19
Clakatakito <alexserranolledo@gmail.com>
2024-11-09 16:55:00 +0100

    acabe

---
+++ programacion/JavaScript/operadores/comparadores_extrictos.js
@@ -0,0 +1,11 @@
+let numero1 = 20
+let numero2 = 20
+
+console.log(numero1 == numero2)
+console.log(numero1 === numero2)
+
+numero1 = 20
+numero2 = "20"
+
+console.log(numero1 == numero2)
+console.log(numero1 === numero2)
