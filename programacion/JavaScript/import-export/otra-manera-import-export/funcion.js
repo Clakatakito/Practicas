1de4ef667beccb5b94d7ae90a24e102f22d781bc
Clakatakito <alexserranolledo@gmail.com>
2024-11-19 23:08:15 +0100

    import-export

---
+++ programacion/JavaScript/import-export/otra-manera-import-export/funcion.js
@@ -0,0 +1,8 @@
+
+//Funciones
+
+//export default function multiplicar(n1,n2){
+    //return n1 * n2;
+//};
+const multiplicar = (n1,n2) => n1*n2
+export default multiplicar
---
+++ programacion/JavaScript/import-export/otra-manera-import-export/main.js
@@ -0,0 +1,3 @@
+import multiplicar from "./funcion.js"
+
+console.log(multiplicar(2,3))
