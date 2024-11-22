92f720acc943b5c7370cab640a7d63dd7f4cccd2
Clakatakito <alexserranolledo@gmail.com>
2024-11-22 14:35:19 +0100

    fech

--- programacion/JavaScript/fech/Fetch-API-con-Promises.js
+++ programacion/JavaScript/fech/Fetch-API-con-Promises.js
@@ -1,3 +1,20 @@
 
 //Fetch API con Promises
 
+const url = "https://jsonplaceholder.typicode.com/comments"
+
+fetch(url)
+    .then((response) =>{
+        if(response.ok){
+            return response.json()
+        }if(!response.ok){
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+    })
+    .then(data =>{
+        console.log(data)
+    })
+    
+    .catch(error =>{
+        console.log(error.message)
+    })//el .catch solo funciona cuando hay error de conexion
