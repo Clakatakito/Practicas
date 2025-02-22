406a961bade53e62b12d7c809db2c261fe75fee3
Clakatakito <alexserranolledo@gmail.com>
2025-02-22 13:06:19 +0100

    bastante

--- programacion/Front-end/JavaScript/Proyectos/envio_emails/js/app.js
+++ programacion/Front-end/JavaScript/Proyectos/envio_emails/js/app.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
+    const email = {
+        email: "",
+        asunto: "",
+        mensaje: ""
+    }
     const formulario = document.querySelector("#formulario")
     const inputEmail = document.querySelector("#email")
     const inputAsunto = document.querySelector("#asunto")
     const inputMensaje = document.querySelector("#mensaje")
+    const botonForm = document.querySelector("#formulario button[type='submit']")
 
 
 
@@ -11,10 +17,20 @@ document.addEventListener("DOMContentLoaded", function(){
     inputAsunto.addEventListener("blur", validar)
 
     inputMensaje.addEventListener("blur", validar)
+    botonForm.addEventListener("click", function(){
+        const enviado = document.createElement("P")
+        enviado.textContent = "ENVIADO!!!"
+        enviado.color = "white"
+        enviado.backgroundColor = "green"
+        formulario.appendChild(enviado)
+
+    })
 
     function validar(e){
         if(e.target.value.trim() === ""){
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement)
+            email[e.target.name] = ""
+            comprovarEmail()
             return
         }
         
@@ -22,9 +38,13 @@ document.addEventListener("DOMContentLoaded", function(){
 
         if(e.target.id === "email" && !validarEmai(e.target.value)){
             mostrarAlerta("Email no valido", e.target.parentElement)
+            email[e.target.name] = ""
+            comprovarEmail()
             return
 
         }
+        email[e.target.name] = e.target.value.trim().toLowerCase()
+        comprovarEmail()
         
     }
 
@@ -53,4 +73,16 @@ document.addEventListener("DOMContentLoaded", function(){
         const regexResultado = regex.test(email)
         return regexResultado
     }
-})  
+
+    function comprovarEmail(){
+        if(Object.values(email).includes("")){
+            botonForm.classList.add("opacity-50")
+            botonForm.disable = true
+
+        }else{
+            botonForm.classList.remove("opacity-50")
+            botonForm.disable = false
+        }
+    }
+
+})
