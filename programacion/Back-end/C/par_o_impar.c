c63daf8f5ca75ab6adda3344394252a8d770c4e6
Alex <157515741+Clakatakito@users.noreply.github.com>
2025-11-04 17:20:40 +0100

    Create par_o_impar.c

---
+++ programacion/Back-end/C/par_o_impar.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Escribeme un numerin y te digo si es para o no!\n");
+    int numerin;
+
+    scanf("%d", &numerin);
+
+    if(numerin % 2 == 0){
+        printf("es cabron\n");
+    }else{
+
+        printf("no es cabron\n");
+
+    }
+
+
+
+
+
+    return 0;
+}
