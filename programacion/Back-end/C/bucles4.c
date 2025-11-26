32a6696ef0fb2af43a664043ae030a8d358fe110
Alex <157515741+Clakatakito@users.noreply.github.com>
2025-11-26 17:56:06 +0100

    Update bucles4.c

--- programacion/Back-end/C/bucles4.c
+++ programacion/Back-end/C/bucles4.c
@@ -12,9 +12,13 @@ int main()
     printf("Dime un numerin y te hago la cuenta atras!\n");
     scanf("%d", &numerin);
     printf("Cuenta atras\n");
-    for (int i = numerin; i != -1; i--)
+    for (int i = numerin; i != -1; i--){
 
-    printf("%d\n", i);
+        printf("%d\n", i);
+
+    }
+
+
 
     return 0;
 }
