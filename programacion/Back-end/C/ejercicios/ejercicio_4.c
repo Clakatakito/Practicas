3cb3714052b9bf17e5f3eadadf4674609226ae59
Alex <alexserranolledo@gmail.com>
2026-01-13 17:33:07 +0100

    el ejercicio 4

---
+++ programacion/Back-end/C/ejercicios/ejercicio_4.c
@@ -0,0 +1,53 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+
+
+
+    int intento = 4;
+    int cont = 0;
+    int respuesta;
+    bool correcto = false;
+
+
+    printf("Introduce un numero positivo i divisible\n");
+
+
+
+    while(cont != 4 && correcto == false){
+
+
+        cont++;
+        scanf("%d", &respuesta);
+
+
+
+
+        printf("Sigue poniendo, te quedan %d\n", cont);
+
+
+
+        if(respuesta % 5 == 0){
+
+
+            correcto = true;
+
+            if(cont == 4){
+
+                printf("Que buena, a la ultima");
+                correcto = true;
+
+            }
+
+
+        }
+
+
+    }
+
+    return 0;
+}
